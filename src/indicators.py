a18b75c9706ee6e127db8a7ce21484280cfa0cdd
Arda Keskiner <keskinerarda@Ardas-MacBook-Pro.local>
2016-11-02 20:40:34 +0300

    Basic Indicators

---
+++ src/indicators.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+
+def simple_moving_average(series, window=14):
+    return series.rolling(window).mean()
+
+
+def exponential_moving_average(series, window=14):
+    return series.ewm(span=window).mean()
+
+
+def relative_strength_index(series, window=14, average_type="SMA"):
+    delta = series.fillna(0).diff()[1:]
+    ups = delta.copy()
+    downs = delta.copy()
+    ups[ups < 0] = 0
+    downs[downs > 0] = 0
+
+    if average_type == "EMA":
+        rolling_ups = exponential_moving_average(ups, window)
+        rolling_downs = exponential_moving_average(downs.abs(), window)
+    else:
+        rolling_ups = simple_moving_average(ups, window)
+        rolling_downs = simple_moving_average(downs.abs(), window)
+
+    rs = rolling_ups / rolling_downs
+    rsi = 100.0 - (100.0 / (1.0 + rs))
+
+    return rsi
+
+
+def average_true_range(df, window=14):
+    new_df = pd.DataFrame()
+    new_df["atr1"] = (df["High"] - df["Low"]).abs()
+    new_df["atr2"] = (df["High"] - df["Adj Close"].shift()).abs()
+    new_df["atr3"] = (df["Low"] - df["Adj Close"].shift()).abs()
+    return new_df[["atr1", "atr2", "atr3"]].max(axis=1)
+
+
+def average_directional_index(df, window=14):
+    up_move = df["High"].fillna(0).diff()
+    down_move = df["Low"].fillna(0).diff()
+    plus_dm = up_move.copy()
+    minus_dm = down_move.copy()
+    plus_dm[up_move < down_move] = 0
+    plus_dm[up_move < 0] = 0
+
+    minus_dm[down_move < up_move] = 0
+    minus_dm[down_move < 0] = 0
+
+    atr = average_true_range(df)
+
+    plus_di = 100 * simple_moving_average(plus_dm, window).fillna(0) / atr
+    minus_di = 100 * simple_moving_average(minus_dm, window).fillna(0) / atr
+
+    return 100 * simple_moving_average((plus_di - minus_di).abs() / (plus_di + minus_di), window)
--- src/test.py
+++ src/test.py
@@ -1,12 +1,83 @@
 import requests
 import csv
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+import indicators
 
-r = requests.get('http://chart.finance.yahoo.com/table.csv?s=TKGBY&a=5&b=3&c=2008&d=9&e=12&f=2016&g=d&ignore=.json')
 
+###
+import datetime
+
+import pandas as pd
+from pandas_datareader import data, wb
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+# Getting Data
+# r = requests.get('http://chart.finance.yahoo.com/table.csv?s=TKGBY&a=5&b=3&c=2008&d=9&e=12&f=2016&g=d&ignore=.json')
+#
 # print(r.text)
 
-reader = csv.reader(r.text.split('\n'), delimiter=',')
-for row in reader:
-    print('\t'.join(row))
+# aapl = data.get_data_yahoo('TKGBY',
+#                                  start=datetime.datetime(2006, 10, 1),
+#                                  end=datetime.datetime(2016, 10, 12))
+# print(aapl)
 
+#---------
+
+
+df = pd.read_csv('data.csv') #, index_col='Date', parse_dates=True)
+df['Date'] = pd.to_datetime(df.Date)
+df = df.sort('Date')
+df = df.set_index('Date')
+# print(df)
+# df['Date'] = pd.to_datetime(df['Date'])
+# df['Date'] = pd.to_datetime(df['Date'])
+# print(df[['Open', 'Close']].head())
+# dff = pandas.DataFrame(df)
+# dff.plot(x='Date', y='Open')
+
+# print(df['Open'])
+
+# plt.plot(df['Date'], df['Close'], linestyle='-', linewidth=1)
+# plt.plot(df['Date'], df['Open'], linestyle='-', linewidth=1)
+# plt.legend(['Close', 'Open'], loc='upper left')
+#
+
+# mavg = Series(df).rolling(window=40, center=False).mean()
+# mavg = pd.rolling_mean(df['Close'], 40)
+# mavg.plot(label='mavg')
+# plt.legend()
+# mavg = pd.rolling_mean(df['Close'], 40)
+# print(mavg[-10:])
+
+# This line is necessary for the plot to appear in a Jupyter notebook
+# % matplotlib inline
+# Control the default size of figures in this Jupyter notebook
+# % pylab inline
+# pylab.rcParams['figure.figsize'] = (15, 9)  # Change the size of plots
+
+df['Adj Close'].plot(grid=True)
+# df['Emwa'] = pd.ewma(df['Close'],span=60,freq='D')
+# df['SMA']  = indicators.simple_moving_average(df, 4)
+# df['EWMA']  = indicators.exponential_moving_average(df['Close'], 40)
+# df['SMA'] = df['Close'].ewm()
+# print(df['SMA'])
+# df['EWMA'].plot(grid=True)
+# df['SMA'].plot(grid=True)
+# mavg = pd.ewma(df['Close'])
+# mavg.plot(grid=True)
+
+# df['RSI'] = indicators.relative_strength_index(df['Close'])
+
+# df['RSI'].plot(grid=True)
+# plt.show()
+
+# print(df['RSI'])
+
+df["ADX"] = indicators.average_directional_index(df, 14)
+df['ADX'].plot(grid=True)
+plt.show()
+
+# print(df["ADX"])
